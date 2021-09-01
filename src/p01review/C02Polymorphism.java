30a51f8a68a0f105e1ea3a71e5fdafc02f968f7d
YoonJuGwang <nanalga@naver.com>
2021-09-01 12:21:36 +0900

    C02Polymorphism(다형성)

---
+++ src/p01review/C02Polymorphism.java
@@ -0,0 +1,43 @@
+package p01review;
+
+public class C02Polymorphism {
+	// 다형성
+	public static void main(String[] args) {
+		Animal a1 = new Animal();
+		Cat c1  = new Cat();
+		Dog d1 = new Dog();
+		
+		a1.cry();
+		c1.cry();
+		d1.cry();
+		
+		System.out.println("=========================");
+		
+		Animal a2 = c1;
+		Animal a3 = d1;
+		
+		a2.cry();
+		a3.cry();
+	}
+
+}
+
+
+
+class Animal{
+	
+	public void cry() {
+		System.out.println("동물이 웁니다.");
+	}
+}
+
+class Cat extends Animal {
+	@Override
+	public void cry() {
+		System.out.println("고양이가 웁니다.");
+	}
+}
+
+class Dog extends Animal {
+
+}
